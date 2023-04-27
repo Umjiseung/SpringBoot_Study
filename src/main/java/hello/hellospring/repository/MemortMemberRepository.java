4c4224aa7928d35af1eda3a65a6dbf9ed9b5a9a5
엄지성 <eom071005123@gmail.com>
2023-04-27 22:59:58 +0900

    정적 컨텐츠부터 API까지 정리

---
+++ src/main/java/hello/hellospring/repository/MemortMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemortMemberRepository implements  MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    public static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.of(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
